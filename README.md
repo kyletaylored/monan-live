7ec53ef9e135d780c9c323490c6674a6ecac240c
Kyle Taylor <kyletaylored@gmail.com>
2021-12-15 18:31:50 -0600

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Monan Live!
+
+**Monan Live!** is a motion detector and image upload project. Using a Raspberry Pi Zero, we detect motion, take a picture, and upload it to a Drupal 9 website. 
+
+
+![Monan Live Fritzing Diagram](python-app/Monan_Live_bb.jpg)
+
+
+
+Part List
+
+- [Raspberry Pi Zero W](https://amzn.to/3dVpFX4)
+- [Arducam 5MP Camera](https://amzn.to/3pZB1il)
+- 
---
+++ python-app/index.py
@@ -0,0 +1,178 @@
+from gpiozero import MotionSensor
+from picamera import PiCamera
+import base64
+import datetime
+import json
+import os
+import pathlib
+import pprint
+import random
+import time
+import requests
+
+# Load environmental variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Set up printer and text generator
+pp = pprint.PrettyPrinter(indent=4)
+f = open('phrases.json')
+phrases = json.load(f)
+f.close()
+phrase = random.choice(phrases['data'])
+
+# Get Drupal host and global auth
+global url
+url = os.getenv('DRUPAL_HOST', 'https://live-monan-live.pantheonsite.io')
+global drupal_auth
+
+# Authenticate with Drupal site
+auth_data = {
+    'name': os.getenv('DRUPAL_USER', 'superuser'),
+    'pass': os.getenv('DRUPAL_PASS', 'password'),
+}
+auth_string = auth_data['name'] + ":" + auth_data['pass']
+
+# Create object for PIR sensor
+# PIR sensor is connected to GPIO-4 (pin 7)
+pir = MotionSensor(4)
+
+# Create Object for PiCamera
+camera = PiCamera()
+camera.rotation = 180
+
+
+# Create filename from date and time.
+def get_file_name():
+    now = datetime.datetime.now()
+    f_name = now.strftime("%Y-%m-%d_%H-%M-%S.jpg")
+    return f_name
+
+
+def get_base64_encoded_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
+
+def monan_auth():
+    # See if we're logged in
+    session_request = requests.get(url + '/session/token')
+    if session_request.status_code == 200 and session_request.text is not None:
+        # We're logged in, return the token
+        return session_request.text
+
+    # Make request, get back CSRF and logout tokens.
+    auth_request = requests.post(url + '/user/login?_format=hal_json', data=auth_data)
+    drupal_auth = auth_request.json()
+
+    # Check if session is already open
+    if auth_request.status_code == 403:
+        if drupal_auth['message'] == "This route can only be accessed by anonymous users.":
+            # Get new session token
+            session_request = requests.get(url + '/session/token')
+            return session_request.text
+
+    # Return session token
+    return drupal_auth['csrf_token']
+
+
+def monan_file(f_name, article_id):
+    # Get base64 image
+    # image_data = get_base64_encoded_image(filename)
+    # image_extension = pathlib.Path(filename).suffix
+    image_name = pathlib.Path(f_name).name
+
+    # Prepare data for POST
+    file = open(f_name, "rb")
+    binary_data = file.read()
+    file.close()
+
+    # POST /jsonapi/node/article/{uuid}/field_image HTTP/1.1
+    # Content-Type: application/octet-stream
+    # Accept: application/vnd.api+json
+    # Content-Disposition: file; filename="filename.jpg"
+
+    # Headers
+    headers = {
+        "Content-Type": "application/octet-stream",
+        "Accept": "application/vnd.api+json",
+        "Content-Disposition": 'file; filename="' + image_name + '"',
+        "X-CSRF-Token": monan_auth(),
+        "Authorization": "Basic " + base64.b64encode(auth_string.encode('utf-8')).decode('utf-8'),
+    }
+    image_response = requests.post(url + '/jsonapi/node/article/' + article_id + '/field_image', data=binary_data,
+                                   headers=headers)
+    return image_response
+
+
+def send_to_monan_live(f_name):
+    # Prepare article data.
+    headers = {
+        "Content-Type": "application/vnd.api+json",
+        "Accept": "application/vnd.api+json",
+        "X-CSRF-Token": monan_auth(),
+        "Authorization": "Basic " + base64.b64encode(auth_string.encode('utf-8')).decode('utf-8'),
+    }
+    data = json.dumps({
+        "data": {
+            "type": "node--article",
+            "attributes": {
+                "title": "Mike: " + phrase['phrase'],
+                "body": {
+                    "value": phrase['meaning'],
+                    "format": "plain_text"
+                }
+            }
+        }
+    })
+
+    # Create new node
+    article_response = requests.post(url + '/jsonapi/node/article', data=data, headers=headers)
+    article_id = article_response.json()['data']['id']
+    pp.pprint(article_response.json())
+
+    # Upload image.
+    image_response = monan_file(f_name, article_id)
+    pp.pprint(image_response.json())
+
+
+# Run main
+def main():
+    print("Starting Mike detection...")
+    while True:
+        # Get filename, set to tmp
+        filename = get_file_name()
+
+        # Wait for motion to be detected
+        pir.wait_for_motion()
+        print("Mike alert!")
+
+        # Wait 2 seconds
+        time.sleep(2)
+
+        # Preview camera on screen until picture is taken
+        # Only used if using desktop OS.
+        # camera.start_preview()
+
+        # Take picture, save to temp
+        print("Taking photo: " + filename)
+        camera.capture(filename)
+        # camera.stop_preview()
+
+        # Send to monan.live
+        send_to_monan_live(filename)
+
+        # Clean files up
+        os.remove(filename)
+
+        # Wait 10 seconds before checking the motion sensor again.
+        time.sleep(30)
+
+
+# Run infinitely
+while error:
+    try:
+        main()
+        error = False
+    except IOError:
+        error = True
